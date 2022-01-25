e8fbf5ce3c25dedfc488fbfbb2d1a4c6ee86d35b
xpoh <laddr@mail.ru>
2022-01-25 08:38:50 +0300

    add prometheus service in compose

---
+++ scripts/start_grafana.sh
@@ -0,0 +1,10 @@
+docker stop grafana
+docker rm grafana
+
+docker run \
+    -d \
+    -p 9090:9090 \
+    -v ~/projects/GB/opt_go_app/lesson-1/scripts/config:/etc/prometheus \
+    --name prometheus \
+    prom/prometheus
+
