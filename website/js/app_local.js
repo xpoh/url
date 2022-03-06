ec23a5cc527b882ae6e442026f17c2a95f2b1659
laddr@mail.ru <SE8sQGgNpjUMuQy>
2022-03-06 10:38:49 +0300

    add local html for testing

---
+++ website/js/app_local.js
@@ -0,0 +1,20 @@
+function logSubmit(event) {
+    event.preventDefault();
+
+    async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    }
+
+    postData('http://localhost:8000/butty/', { "url": document.getElementById('longLink').value })
+        .then((data) => {
+            document.getElementById('longLink').value = data.url;
+        });
+}
+const form = document.getElementById('urlSenderForm');
+form.addEventListener('submit', logSubmit);
